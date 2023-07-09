52009b57c32d10b15854978ee2f351f06232200c
Tino <d_kb@live.cn>
2023-07-09 22:18:07 +0800

    limit

---
+++ LimitDemo.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy
+
+x = np.array(np.linspace(-100, 100, 10000))
+y =  np.sin(x)/x
+
+plt.figure(figsize=(12, 3))
+
+
+plt.plot(x, y)
+
+plt.axvline(x=0, linestyle='--')
+plt.axhline(y=0, linestyle='--')
+
+plt.show()
+
+x1 = sympy.Symbol('x')
+y1 = sympy.sin(x1)/x1
+
+r = sympy.limit(y1, x1, 'oo')
+print(f"function {y1} the limit is {r}")
